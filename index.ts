716b33bd2508ba2e7ba57d1453566c2118696eb7
Natalie Cuthbert <natalie@stitch.money>
2022-04-04 13:17:44 +0200

    Bumped version

--- index.ts
+++ index.ts
@@ -8,8 +8,9 @@ import type { Decoder } from 'decoders';
 
 
 const argv = yargs(hideBin(process.argv)).argv;
-const argsDecoder = decoders.object({
-    tag: decoders.maybe(decoders.string)
+const argsDecoder: Decoder<DecoderProps> = decoders.object({
+    tag: decoders.maybe(decoders.string),
+    namespacedImport: decoders.maybe(decoders.boolean)
 });
 const decodedArgs = argsDecoder.verify(argv);
 
@@ -21,11 +22,18 @@ function decoderNameFromTypeName(name: string) {
     return `${camelCase(name)}Decoder`;
 }
 
-function generateDecoderImport() {
+function generateDecoderImport(props: DecoderProps, imports: { [key: string]: boolean }) {
     const decoderIdentifier = ts.factory.createIdentifier('decoders');
-    const decoderNamespaceImport = ts.factory.createNamespaceImport(decoderIdentifier);
-    const decoderImportClause = ts.factory.createImportClause(false, undefined, decoderNamespaceImport);
     const decoderModuleSpecifier = ts.factory.createStringLiteral('decoders');
+    let decoderImportClause;
+    if (props.namespacedImport) {
+        const decoderNamespaceImport = ts.factory.createNamespaceImport(decoderIdentifier);
+        decoderImportClause = ts.factory.createImportClause(false, undefined, decoderNamespaceImport);
+    } else {
+        const importSpecifiers = Object.keys(imports).map((key) => ts.factory.createImportSpecifier(false, undefined, ts.factory.createIdentifier(key)));
+        const decoderNamedBindings = ts.factory.createNamedImports(importSpecifiers);
+        decoderImportClause = ts.factory.createImportClause(false, undefined, decoderNamedBindings);
+    }
     const decoderImportDeclaration = ts.factory.createImportDeclaration(undefined, undefined, decoderImportClause, decoderModuleSpecifier);
     return decoderImportDeclaration;
 }
@@ -40,21 +48,25 @@ function generateDecoderTypeImport() {
     return decoderImportDeclaration;
 }
 
-function generateDecoderCall(decoderName: string) {
+function generateDecoderCall(decoderName: string, props: DecoderProps, imports: { [key: string]: boolean }) {
     const importIdentifier = ts.factory.createIdentifier('decoders');
     const decoderIdentifier = ts.factory.createIdentifier(decoderName);
-    const propertyAccess = ts.factory.createPropertyAccessExpression(importIdentifier, decoderIdentifier);
-    return propertyAccess;
+    imports[decoderName] = true;
+    if (props.namespacedImport) {
+        const propertyAccess = ts.factory.createPropertyAccessExpression(importIdentifier, decoderIdentifier);
+        return propertyAccess;
+    } else {
+        return decoderIdentifier;
+    }
 }
 
-function generateDecoderFuncCall(decoderName: string, argumentsArray: readonly ts.Expression[] | undefined) {
-    const propertyAccess = generateDecoderCall(decoderName);
+function generateDecoderFuncCall(decoderName: string, argumentsArray: readonly ts.Expression[] | undefined, props: DecoderProps, imports: { [key: string]: boolean }) {
+    const propertyAccess = generateDecoderCall(decoderName, props, imports);
     const callExpression = ts.factory.createCallExpression(propertyAccess, undefined, argumentsArray);
     return callExpression;
 }
-decoders.taggedUnion
 
-function decoderFromUnionType(unionType: ts.UnionTypeNode, props: { tag?: string }) {
+function decoderFromUnionType(unionType: ts.UnionTypeNode, props: DecoderProps, imports: { [key: string]: boolean }) {
     if (props.tag) {
         if (unionType.types.every(subtype => {
             if (subtype.kind === ts.SyntaxKind.TypeLiteral) {
@@ -87,7 +99,7 @@ function decoderFromUnionType(unionType: ts.UnionTypeNode, props: { tag?: string
                         unionType.types
                             .map(x => x as ts.TypeLiteralNode)
                             .map(subtype => {
-                                let prop = decoderFromType(subtype, props);
+                                let prop = decoderFromType(subtype, props, imports);
 
                                 if (!prop) {
                                     return;
@@ -100,39 +112,41 @@ function decoderFromUnionType(unionType: ts.UnionTypeNode, props: { tag?: string
                             }).filter(x => !!x) as ts.ObjectLiteralElementLike[],
                         true
                     )
-                ]);
+                ], props, imports
+            );
         }
     }
-    const subtypes: ts.Expression[] = unionType.types.map(x => decoderFromType(x, props)).filter(x => !!x) as ts.Expression[];
-    return generateDecoderFuncCall('either', subtypes);
+    const subtypes: ts.Expression[] = unionType.types.map(x => decoderFromType(x, props, imports)).filter(x => !!x) as ts.Expression[];
+    return generateDecoderFuncCall('either', subtypes, props, imports);
 }
-type DecoderProps = { tag?: string };
 
-function decoderFromType(type: ts.TypeNode, props: DecoderProps): ts.Expression | undefined {
+type DecoderProps = { tag?: string | null, namespacedImport?: boolean | null };
+
+function decoderFromType(type: ts.TypeNode, props: DecoderProps, imports: { [key: string]: boolean }): ts.Expression | undefined {
     switch (type?.kind) {
         case ts.SyntaxKind.StringKeyword:
-            return generateDecoderCall('string');
+            return generateDecoderCall('string', props, imports);
 
         case ts.SyntaxKind.NumberKeyword:
-            return generateDecoderCall('number');
+            return generateDecoderCall('number', props, imports);
 
         case ts.SyntaxKind.BooleanKeyword:
-            return generateDecoderCall('boolean');
+            return generateDecoderCall('boolean', props, imports);
 
         case ts.SyntaxKind.ObjectKeyword:
-            return generateDecoderCall('jsonObject');
+            return generateDecoderCall('jsonObject', props, imports);
         case ts.SyntaxKind.UnionType:
-            return decoderFromUnionType(type as ts.UnionTypeNode, props);
+            return decoderFromUnionType(type as ts.UnionTypeNode, props, imports);
         case ts.SyntaxKind.ArrayType:
-            const arrayType = decoderFromType((type as ts.ArrayTypeNode).elementType, props);
+            const arrayType = decoderFromType((type as ts.ArrayTypeNode).elementType, props, imports);
             if (!arrayType) {
                 return undefined;
             }
-            return generateDecoderFuncCall('array', [arrayType]);
+            return generateDecoderFuncCall('array', [arrayType], props, imports);
         case ts.SyntaxKind.TupleType:
             const tupleType = type as ts.TupleTypeNode;
-            const elements = tupleType.elements.map(x => decoderFromType(x, props)).map(x => x ?? generateDecoderCall('json')) as ts.Expression[];
-            return generateDecoderFuncCall('tuple', elements);
+            const elements = tupleType.elements.map(x => decoderFromType(x, props, imports)).map(x => x ?? generateDecoderCall('json', props, imports)) as ts.Expression[];
+            return generateDecoderFuncCall('tuple', elements, props, imports);
             return undefined;
         case ts.SyntaxKind.LiteralType:
             const literal = (type as ts.LiteralTypeNode).literal;
@@ -147,7 +161,7 @@ function decoderFromType(type: ts.TypeNode, props: DecoderProps): ts.Expression 
             if (!arg) {
                 return undefined;
             }
-            return generateDecoderFuncCall('constant', [arg]);
+            return generateDecoderFuncCall('constant', [arg], props, imports);
 
         case ts.SyntaxKind.TypeReference:
             const typename = (type as ts.TypeReferenceNode).typeName.getText();
@@ -158,32 +172,33 @@ function decoderFromType(type: ts.TypeNode, props: DecoderProps): ts.Expression 
             const propSigs = typeLiteralNode.members.map(x => x.kind === ts.SyntaxKind.PropertySignature ? x as ts.PropertySignature : undefined).filter(x => !!x) as ts.PropertySignature[];
 
             const objectLiteralExpr = ts.factory.createObjectLiteralExpression(propSigs.map(x => {
-                let prop = decoderFromType(x.type!, props);
+                let prop = decoderFromType(x.type!, props, imports);
 
                 if (!prop) {
                     return;
                 }
                 if (x.questionToken) {
-                    prop = generateDecoderFuncCall('optional', [prop]);
+                    prop = generateDecoderFuncCall('optional', [prop], props, imports);
                 }
                 return ts.factory.createPropertyAssignment(x.name, prop);
             }).filter(x => !!x) as ts.ObjectLiteralElementLike[], true
             );
-            return generateDecoderFuncCall('object', [objectLiteralExpr]);
+            return generateDecoderFuncCall('object', [objectLiteralExpr], props, imports);
         default:
             return undefined;
     }
 }
 
 
-function generateDecoder(decl: ts.TypeAliasDeclaration, props: DecoderProps) {
+function generateDecoder(decl: ts.TypeAliasDeclaration, props: DecoderProps, imports: { [key: string]: boolean }) {
     const typeName = decl.name.escapedText.toString();
     const decoderName = ts.factory.createIdentifier(decoderNameFromTypeName(typeName));
 
-    const objectDecoderCall = decoderFromType(decl.type, props);
+
+    const objectDecoderCall = decoderFromType(decl.type, props, imports);
 
     const typeDeclaration = ts.factory.createTypeReferenceNode(typeName, []);
-    const decoderTypeDeclaration = ts.factory.createTypeReferenceNode('decoders.Decoder', [typeDeclaration]);
+    const decoderTypeDeclaration = ts.factory.createTypeReferenceNode('Decoder', [typeDeclaration]);
 
     const decoderDeclaration = ts.factory.createVariableDeclaration(decoderName, undefined, decoderTypeDeclaration, objectDecoderCall);
 
@@ -196,16 +211,17 @@ function generateDecoder(decl: ts.TypeAliasDeclaration, props: DecoderProps) {
 
 export function generate(sourceFile: ts.SourceFile, props: DecoderProps) {
     const generatedAstNodes: (ts.Node | undefined)[] = [];
-    generatedAstNodes.push(generateDecoderImport());
-    generatedAstNodes.push(generateDecoderTypeImport());
-    generatedAstNodes.push(undefined);
+    const imports: { [key: string]: boolean } = {};
     ts.forEachChild(sourceFile, processNode);
+    generatedAstNodes.unshift(undefined);
+    generatedAstNodes.unshift(generateDecoderImport(props, imports));
+    generatedAstNodes.unshift(generateDecoderTypeImport());
 
 
     function processNode(node: ts.Node) {
         switch (node.kind) {
             case ts.SyntaxKind.TypeAliasDeclaration:
-                const decoder = generateDecoder(node as ts.TypeAliasDeclaration, props);
+                const decoder = generateDecoder(node as ts.TypeAliasDeclaration, props, imports);
                 generatedAstNodes.push(decoder);
                 generatedAstNodes.push(undefined);
                 break;
@@ -222,7 +238,6 @@ export function generate(sourceFile: ts.SourceFile, props: DecoderProps) {
             process.stdout.write('\n');
         }
     });
-
 }
 
 
@@ -235,5 +250,5 @@ if (require.main === module) {
         true,
         ts.ScriptKind.TS
     );
-    generate(sourceFile, { tag: decodedArgs.tag ?? undefined });
+    generate(sourceFile, { ...decodedArgs, tag: decodedArgs.tag ?? undefined });
 }
